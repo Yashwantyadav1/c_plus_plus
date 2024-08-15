42efd8b3dbf2e2b0d59a3b5020fdf01dc67b5f45
Yashwantyadav1 <yy888275@gmail.com>
2024-08-15 18:41:35 +0530

    numeric hollow pyramid

---
+++ 04_Basic/Pattern_question/03_numeric_hollow_pyramid.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int row=0; row<n; row++){
+        // Space print 
+        for(int col=0; col<n-row-1; col++){
+            cout<<" ";
+        }
+        // in this line we print number with Space 
+        int start=1;
+        for(int col=0; col<2*row+1; col++){
+            if(col==0||row== n-1){
+                if(col%2==0){
+                 cout<<start;
+                  start=start+1;
+                }
+                else{ 
+                  cout<<" ";
+                }
+             
+            }
+            // in this line we print 1 in 1st pyramid row 
+            else{
+                if(col==0){
+                    cout<<1;
+                }
+                // in this line we print row +1 count in the last pyramid 
+                else if(col==2*row){
+                    cout<<row+1;
+
+                }
+                // in this line we print space between them
+                else{
+                    cout<<" ";
+                }
+            }
+            
+        }
+        cout<<endl;
+    }
+}
