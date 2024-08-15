a55e7238a074acae1f1138bcaa010c7652fa0063
Yashwantyadav1 <yy888275@gmail.com>
2024-08-15 16:51:29 +0530

    half pyramid by start counting with row

---
+++ 04_Basic/Pattern_question/01_half_pyramid.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    for(int row=0; row<n; row++){
+        int start = row+1;
+        for(int col=0; col<row+1; col++){
+            cout<<start <<" ";
+            start = start+1;
+        }
+        cout<<endl;
+    }
+}
