5a89155cefad5af9ce755b4d207cf1a721289394
Yashwantyadav1 <yy888275@gmail.com>
2024-08-15 18:46:26 +0530

    Full pyramid with include Space ,Normal counting and Inverse counting

--- 04_Basic/Pattern_question/02_full_pyramid.cpp
+++ 04_Basic/Pattern_question/02_full_pyramid.cpp
@@ -5,12 +5,15 @@ int main(){
     int n;
     cin>>n;
     for(int row=0; row<n; row++){
+        // in this line we print space inverted pyramid 
         for(int col=0; col<n-row-1; col++){
             cout<<" ";
         }
+        //in this line we print numeric pyramid 
         for(int col=0; col<row+1; col++){
             cout<<row+col+1;
         }
+        //in this line we print inverse counting in numeric pyramid 
         int start=2*row;
         for(int col=0; col<row; col=col+1){
             cout<<start;
