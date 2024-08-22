98feef2acbe63b28d62dfe851bb9618a5efa33ba
Yashwantyadav1 <yy888275@gmail.com>
2024-08-22 12:46:04 +0530

    update

--- class_work/sort_array.cpp
+++ class_work/sort_array.cpp
@@ -1,31 +1,24 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
     int arr[] = {1, 3, 5, 8};
-    
-    int n = sizeof(arr) / sizeof(arr[0]); 
-
-    for(int i = 1; i < n; i++) {
-        if(arr[i-1] <= arr[i]) {
-            continue;
-        } else {
-            cout << "Array is not sorted" << endl;
-            return 0; 
-        }
-    }
-    cout << "Array is sorted" << endl;
-
     int arr1[]={1,4,5,7,8,9};
-     n= sizeof(arr1) / sizeof(arr1[0]);
-    for(int i=1;i<n;i++){
-        if(arr1[i-1] <= arr1[i]){
-            continue;
-        }else{
-            cout<<"Array is not sorted"<< endl;
-            return 0;
-        }
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int m = sizeof(arr1) / sizeof(arr1[0]);
+
+    
+    map<int ,int> m1;
+    for(int i=0; i<n; i++){
+        m1[arr[i]]++;
+        
     }
-    cout<<"Array is sorted"<<endl;
+    for(int j=0;j<m;j++){
+        m1[arr1[j]]++;
+    }
+    for(auto it:m1) {
+        cout<<it.first<<endl;
+    }
     return 0;
+
 }
