d192e0da33af1b09af462adefd8800be0faedb08
Yashwantyadav1 <yy888275@gmail.com>
2024-08-22 21:36:23 +0530

    incremental number triangle

--- 04_Basic/Pattern_question/04_incremental_number_triangle.cpp
+++ 04_Basic/Pattern_question/04_incremental_number_triangle.cpp
@@ -4,10 +4,11 @@ int main(){
     int n;
     cin>>n;
     for(int row=0; row<n; row++){
-        int start=row+1;
+        // int start=row+1;
         for(int col=0; col<row+1; col++){
-            cout<<start;
-            start=start+1;
+            // cout<<start;
+            // start=start+1;
+            cout<<row+col+1<<" ";
         }
         cout<<endl;
     }
