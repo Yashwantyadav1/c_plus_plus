59f9b03952b718e3601c8f67ce7f8ea01fb86d6c
Yashwantyadav1 <yy888275@gmail.com>
2024-08-22 19:53:53 +0530

    full pyramid

--- 04_Basic/Pattern_question/04_full_pyramid.cpp
+++ 04_Basic/Pattern_question/04_full_pyramid.cpp
@@ -4,8 +4,10 @@ int main(){
     int n;
     cin>>n;
     for(int row=0; row<n; row++){
+        int start=row+1;
         for(int col=0; col<row+1; col++){
-            cout<<row+col+1;
+            cout<<start;
+            start=start+1;
         }
         cout<<endl;
     }
