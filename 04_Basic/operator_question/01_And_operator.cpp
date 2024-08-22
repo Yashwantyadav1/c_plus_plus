49a689bd7a9ede71bfaa8612c73b0bfeaf439fef
Yashwantyadav1 <yy888275@gmail.com>
2024-08-22 23:10:14 +0530

    AND operator

---
+++ 04_Basic/operator_question/01_And_operator.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+//     bool a=true;
+//    // bool b=true;
+//    bool b=false;
+//    /*In the case of and (&) our compilar give if two no. is true it will give True value 
+//     Otherwise it will give False value  */
+//     cout<<( a & b);
+
+    int k=2;
+    int h=3;
+    cout<<(k & h);
+}
