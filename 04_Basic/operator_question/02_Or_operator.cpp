e54cc9451cb399786a02b259786a8a36ef09d3ed
Yashwantyadav1 <yy888275@gmail.com>
2024-08-22 23:18:45 +0530

    OR operator

---
+++ 04_Basic/operator_question/02_Or_operator.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    bool a=true;
+    bool k=true;
+    bool j=false;
+    bool b=false;
+/*In OR operator if the two value is true it return true value 
+And if two value is false it will return false value 
+Otherwise it wiil true*/
+    cout<<(a | b)<<endl;
+    cout<<(a | k)<<endl;
+    cout<<(j | b)<<endl;
+}
