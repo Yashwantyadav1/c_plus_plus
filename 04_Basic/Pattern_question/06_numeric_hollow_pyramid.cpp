dd980e5134d557ea2da849fd528351603344b74b
Yashwantyadav1 <yy888275@gmail.com>
2024-08-22 22:31:50 +0530

    Numeric Holllow pyramid

---
+++ 04_Basic/Pattern_question/06_numeric_hollow_pyramid.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int row=0; row<n; row++){
+        for(int col=0; col<n-row-1; col++){
+            cout<<" ";
+        }
+         int start=1;
+        for(int col=0; col<2*row+1; col++){
+           
+            if(row==0 || row==n-1){
+                if(col%2==0){
+                    cout<<start;
+                    start=start+1;
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            else{
+                if(col==0){
+                    cout<<1;
+                }
+                else if(col==2*row){
+                    cout<<row+1;
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+}
