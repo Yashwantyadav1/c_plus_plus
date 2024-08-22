f53b46c059471707d551917dfb7a681118aab025
Yashwantyadav1 <yy888275@gmail.com>
2024-08-22 23:28:36 +0530

    Xor operator

---
+++ 04_Basic/operator_question/04_Xor_operator.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    bool a=true;
+    bool b=false;
+    bool c=false;
+    bool f=true;
+
+    cout<<(a^b)<<endl;
+    cout<<(b^c)<<endl;
+    cout<<(a^f)<<endl;
+       
+}
+
