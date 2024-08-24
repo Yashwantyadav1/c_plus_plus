7a6842b9afad4eb23c7c6712e6ee4ff501e84193
Yashwantyadav1 <yy888275@gmail.com>
2024-08-24 22:42:22 +0530

    array highest frequency and lowest frequncy

--- class_work/array_freq_highest_lowest.cpp
+++ class_work/array_freq_highest_lowest.cpp
@@ -8,8 +8,7 @@ int main() {
     int n = 5;
     int arr[] = {1, 1, 8, 3, 5};
     map<int, int> m;
-
-    // Counting the frequency of each element
+    
     for (int i = 0; i < n; i++) {
         m[arr[i]]++;
     }
