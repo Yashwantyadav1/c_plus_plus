8997917c1fcfd5aec3126c386a06a2689dadb222
Yashwantyadav1 <yy888275@gmail.com>
2024-08-25 23:06:21 +0530

    Adding Two number

--- BASICS/2_practice.cpp
+++ BASICS/2_practice.cpp
@@ -1,11 +1,17 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main (){
+
     int a,b;
+
     cout<<"enter the number of a"<<endl;
+
     cin>>a;
+
     cout<<"enter the value of b"<<endl;
+
     cin>>b;
+
     cout<<"the sum of the value of a+b is :"<< a+b<< endl;
     
 }
