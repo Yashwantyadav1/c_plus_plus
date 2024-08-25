9f8f09a8335c266c695f257b366cb60f98a5d6b6
Yashwantyadav1 <yy888275@gmail.com>
2024-08-25 22:52:22 +0530

    Half pyramid

--- 04_Basic/Pattern_question/01_half_pyramid.cpp
+++ 04_Basic/Pattern_question/01_half_pyramid.cpp
@@ -4,12 +4,16 @@ int main(){
     int n;
     cin>> n;
     for(int row=0; row<n; row++){
+
         int start = row+1;
+        
         for(int col=0; col<row+1; col++){
+
             cout<<start <<" ";
             // cout<<row+col+1;
             start = start+1;
         }
+
         cout<<endl;
     }
 }
