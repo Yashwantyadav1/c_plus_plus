25b2fb36df6e4845d2055d29ff0fd9a0ec937759
Yashwantyadav1 <yy888275@gmail.com>
2024-08-26 22:20:19 +0530

    Acess string by using string

--- BASICS/7_practice.cpp
+++ BASICS/7_practice.cpp
@@ -1,12 +1,13 @@
-#include<iostream>
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string mystring="hellow";
+    string mystring="Hellow";
     cout<<mystring [0]<<endl ;
     cout<<mystring[1]<<endl;
     cout<<mystring[2]<<endl;
     cout<<mystring[3]<<endl;
+    cout<<mystring[4]<<endl;
+    cout<<mystring[5]<<endl;
 
     return 0;
 
