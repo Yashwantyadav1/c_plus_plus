5b3212476e02b0ed835ac5ce26171f8f4326b3e2
Yashwantyadav1 <yy888275@gmail.com>
2024-08-26 23:51:01 +0530

    increment operator

--- BASICS/multiple_variable.cpp
+++ BASICS/multiple_variable.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main() {
-  int x, y, z;
-  x = y = z = 55;
-  cout << x + y + z;
+  // int x, y, z;
+  // x = y = z = 55;
+  // cout << x + y + z;
+  // its value depand on compilar
+  int a=5;
+  cout<<(++a)*(++a);
   return 0;
 }
