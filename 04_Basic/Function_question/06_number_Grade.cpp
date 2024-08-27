e05f3882b1a3cc000b7e83714604e0a325e88f09
Yashwantyadav1 <yy888275@gmail.com>
2024-08-27 22:50:38 +0530

    Determine the student grade with the help of Function call

--- 04_Basic/Function_question/06_number_Grade.cpp
+++ 04_Basic/Function_question/06_number_Grade.cpp
@@ -1,23 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-void grade(int n){
-    if(n >= 90){
-        cout<<"Your grade is 'A' "<<endl;
+char grade(int marks){
+    // if(n >= 90){
+    //     cout<<"Your grade is 'A' "<<endl;
+    // }
+    // else if(n>=70){
+    //     cout<<"Your grade is 'B' "<<endl;
+    // }
+    // else if(n>=60){
+    //     cout<<"Your grade is 'C' "<<endl;
+    // }
+    // else{
+    //     cout<<"Your grade is 'D' "<<endl;
+    // }
+    switch ( marks/10 ){
+        case9: return 'A';
+        break;
+        case7: return 'B';
+        break;
+        case6: return 'D';
+        break;
+        default: return'E';
     }
-    else if(n>=70){
-        cout<<"Your grade is 'B' "<<endl;
-    }
-    else if(n>=60){
-        cout<<"Your grade is 'C' "<<endl;
-    }
-    else{
-        cout<<"Your grade is 'D' "<<endl;
-    }
+
 }
 int main(){
-    int n;
+    int marks;
     cout<<"Enter the Student Number that he achive in that  Subject  "<<endl;
-    cin>>n;
+    cin>>marks;
 
-    grade(n);
+    char finalgrade=grade( marks);
+    cout<<finalgrade;
+    return 0;
+
 }
