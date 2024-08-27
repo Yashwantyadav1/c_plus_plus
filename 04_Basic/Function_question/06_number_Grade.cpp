1ff385a4b9344c14aedbb09b368245605521f6fe
Yashwantyadav1 <yy888275@gmail.com>
2024-08-27 22:58:45 +0530

    Determine the student grade with the help of Function call

--- 04_Basic/Function_question/06_number_Grade.cpp
+++ 04_Basic/Function_question/06_number_Grade.cpp
@@ -1,36 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-char grade(int marks){
-    // if(n >= 90){
-    //     cout<<"Your grade is 'A' "<<endl;
-    // }
-    // else if(n>=70){
-    //     cout<<"Your grade is 'B' "<<endl;
-    // }
-    // else if(n>=60){
-    //     cout<<"Your grade is 'C' "<<endl;
-    // }
-    // else{
-    //     cout<<"Your grade is 'D' "<<endl;
-    // }
-    switch ( marks/10 ){
-        case9: return 'A';
-        break;
-        case7: return 'B';
-        break;
-        case6: return 'D';
-        break;
-        default: return'E';
+char grade(int n){
+    if(n >= 90){
+        cout<<"Your grade is 'A' "<<endl;
     }
+    else if(n>=70){
+        cout<<"Your grade is 'B' "<<endl;
+    }
+    else if(n>=60){
+        cout<<"Your grade is 'C' "<<endl;
+    }
+    else{
+        cout<<"Your grade is 'D' "<<endl;
+    }
+    // switch ( marks/10 ){
+    //     case9: return 'A';
+    //     break;
+    //     case7: return 'B';
+    //     break;
+    //     case6: return 'D';
+    //     break;
+    //     default: return'E';
+    // }
 
 }
 int main(){
-    int marks;
-    cout<<"Enter the Student Number that he achive in that  Subject  "<<endl;
-    cin>>marks;
+    // int marks;
+    // cout<<"Enter the Student Number that he achive in that  Subject  "<<endl;
+    // cin>>marks;
 
-    char finalgrade=grade( marks);
-    cout<<finalgrade;
+    // char finalgrade=grade( marks);
+    // cout<<finalgrade;
+
+    for(int i=1; i<=100; i++){
+        char ans= grade(i);
+        cout<<"grade for marks = "<< i <<"is "<<ans<<endl;
+    }
     return 0;
 
 }
