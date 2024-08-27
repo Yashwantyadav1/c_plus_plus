ec8b2be36c155e09f47fad1013a39f94c97c000b
Yashwantyadav1 <yy888275@gmail.com>
2024-08-27 17:31:33 +0530

    print num with the help of pass by value & also print its address

--- 04_Basic/Function_question/01_add_two_number.cpp
+++ 04_Basic/Function_question/01_add_two_number.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 void printNum(int a){
     a++;
+    ++a;
+    ++a;
     cout<<a<<endl;
+    
 }
 int main(){
     int a=5;
-    printNum(a)
+    printNum(a);
+
+    cout<<"Address of a is "<< &a <<endl;
 }
