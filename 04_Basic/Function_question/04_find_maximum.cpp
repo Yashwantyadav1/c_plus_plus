607e3c2f65fa526a0eef259036efe929103ee33b
Yashwantyadav1 <yy888275@gmail.com>
2024-08-27 18:16:29 +0530

    To check the Maximum number with the help of function call

---
+++ 04_Basic/Function_question/04_find_maximum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxnum(int num1,int num2,int num3){
+    if()
+}
+int main(){
+    int a,b,c,;
+    cout<<"Enter the value of a"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b"<<endl;
+    cin>>b;
+    cout<<"Enter the value of c"<<endl;
+    cin>>c;
+
+    maximumNumber=maxnum(a,b,c);
+}
