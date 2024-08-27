ef39b08d13aaac044f453b84c55bfce26ceea5f3
Yashwantyadav1 <yy888275@gmail.com>
2024-08-27 22:01:55 +0530

    find maximum number with the help of Function call

--- 04_Basic/Function_question/04_find_maximum.cpp
+++ 04_Basic/Function_question/04_find_maximum.cpp
@@ -1,10 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int maxnum(int num1,int num2,int num3){
-    if()
+int findmaxnum(int num1,int num2,int num3){
+    if(num1>num2 && num1>num3){
+        return num1;
+    
+    }
+    else if(num2>num1 && num2>num3){
+        return num3;
+    }
+    else{
+        return num3;
+    }
 }
 int main(){
-    int a,b,c,;
+    int a,b,c;
     cout<<"Enter the value of a"<<endl;
     cin>>a;
     cout<<"Enter the value of b"<<endl;
@@ -12,5 +21,6 @@ int main(){
     cout<<"Enter the value of c"<<endl;
     cin>>c;
 
-    maximumNumber=maxnum(a,b,c);
+    int maximumNumber = findmaxnum(a,b,c);
+    cout<<maximumNumber;
 }
