3806bfaa94dce5e4991d21a5f3d8003b6d578ad6
Yashwantyadav1 <yy888275@gmail.com>
2024-08-27 22:11:18 +0530

    Print counting from 1 to n with the help of Function  call

---
+++ 04_Basic/Function_question/05_counting_.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void counting(int n){
+    for(int i=1; i<=n; i++){
+
+        cout<<i<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the value of n "<<endl;
+    cin>>n;
+    counting(n);
+}
