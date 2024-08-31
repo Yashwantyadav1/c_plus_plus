6ee69e495f35084639fc59f3c0f18d1be6a8406e
Yashwantyadav1 <yy888275@gmail.com>
2024-08-31 16:37:59 +0530

    Numeric hollow inverted pyramid

--- 04_Basic/Pattern_question/08_Numeric_hollow_inverted_pyramid.cpp
+++ 04_Basic/Pattern_question/08_Numeric_hollow_inverted_pyramid.cpp
@@ -5,7 +5,13 @@ int main(){
     cin>>n;
     for(int row=0; row<n; row++){
         for(int col=row+1; col<=n; col++){
-            cout<<col;
+            if(col==row+1 ||col==n || row==0){
+                cout<<col;    
+            }
+            else{
+                cout<<" ";
+            }
+            
         }
         cout<<endl;
     }
