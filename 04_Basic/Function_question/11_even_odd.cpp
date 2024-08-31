c1e3c595dcd0e47c3c7bbbeca88d21bdb3c96f10
Yashwantyadav1 <yy888275@gmail.com>
2024-09-01 01:09:21 +0530

    To check the number is Even OR Odd Number with the help of Function

---
+++ 04_Basic/Function_question/11_even_odd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int even_number(int n){
+   
+       if(n % 2 == 0){
+        cout<<"This number is Even Number ";
+        }
+      else{
+        cout<<"This number is not Even Number, This is Odd";
+        }
+
+    
+    
+}
+int main(){
+    int n;
+    cout<<"Enter the number "<<endl;
+    cin>>n;
+    int even = even_number(n);
+}
