a05c996b4d8c32502287b90b07b97929fd74c950
Yashwantyadav1 <yy888275@gmail.com>
2024-08-31 21:21:27 +0530

    flody triangle pattern

--- 04_Basic/Pattern_question/12_Flody_triangle_apttern.cpp
+++ 04_Basic/Pattern_question/12_Flody_triangle_apttern.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
     int n;
-    cin>>n;
+    cin>>n; 
+    int c=1;
     for(int i=0; i<n; i++){
-       // int count=1;
+        
         for(int j=0; j<=i; j++ ){
-            cout<<j;
-            // cout<<count;
-            // count++;
+            cout<<c<<" ";
+            c++;
         }
         cout<<endl;
     }
