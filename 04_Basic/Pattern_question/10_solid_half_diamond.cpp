cde8d4cd5b50ffbe308f797961e57ff7225e278b
Yashwantyadav1 <yy888275@gmail.com>
2024-08-31 17:36:26 +0530

    Solid half diamond

--- 04_Basic/Pattern_question/10_solid_half_diamond.cpp
+++ 04_Basic/Pattern_question/10_solid_half_diamond.cpp
@@ -7,9 +7,6 @@ int main(){
         for(int col=0; col<row+1; col++){
             cout<<"*";
         }
-        // for(int col=0; col<n-row-1; col++){
-        //     cout<<"*";
-        // }
         cout<<endl;
     }
     for(int row=0; row<n; row++){
