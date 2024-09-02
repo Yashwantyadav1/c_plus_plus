b2f0d59360316b3b9a4117b6df6b9f0eb64574d0
Yashwantyadav1 <yy888275@gmail.com>
2024-09-02 20:00:43 +0530

    Longest palindrome

--- class_work/Dsa_question/01_longest_palindrome.cpp
+++ class_work/Dsa_question/01_longest_palindrome.cpp
@@ -1,39 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-string longestpalindromesubstring(const string & str){
-    int n = str.length();
-    if(n==0)return "";
-    vector<vector<bool>> dp(n,vector<bool>(n,false));
-    int start = 0;
-    int maxLength =1;
 
-    for(int i=0; i<n; ++i){
-        dp[i][j] = true;
+string longestPalindromeSubstring(const string &str) {
+    int n = str.length();
+    if (n == 0) return "";
+    
+    // Initialize DP table
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int start = 0;
+    int maxLength = 1;
+
+    // Every single character is a palindrome
+    for (int i = 0; i < n; ++i) {
+        dp[i][i] = true;  // Change `dp[i][j]` to `dp[i][i]`
     }
-    for(int i=0; i<n-1; ++i){
-        if(str[i]==str[i+1]){
-            dp[i][i+1]=true;
-            start =i;
-            maxLength=2;
+
+    // Check for a palindrome of length 2
+    for (int i = 0; i < n - 1; ++i) {
+        if (str[i] == str[i + 1]) {
+            dp[i][i + 1] = true;
+            start = i;
+            maxLength = 2;
         }
     }
-    for(int length=3; length<=n; ++length){
-        for(int i=0; i<n-length+1;++i){
-            int j = i+length-1;
-            if(s[i] == s[j] && dp[i+1][j-1]){
-                dp[i][j] = true; 
+
+    // Check for lengths greater than 2
+    for (int length = 3; length <= n; ++length) {
+        for (int i = 0; i < n - length + 1; ++i) {
+            int j = i + length - 1;
+            if (str[i] == str[j] && dp[i + 1][j - 1]) {  // Change `s[i]` to `str[i]`
+                dp[i][j] = true;
                 start = i;
                 maxLength = length;
-
             }
         }
     }
-    return str.substr(start,maxLength);
 
+    return str.substr(start, maxLength);
 }
-int main(){
-    string str="babad";
-    string result =longestpalindromesubstring(str);
-    cout<<"Longest palindrome substring "<<result<<endl;
+
+int main() {
+    string str = "babad";
+    string result = longestPalindromeSubstring(str);
+    cout << "Longest palindrome substring: " << result << endl;
     return 0;
 }
