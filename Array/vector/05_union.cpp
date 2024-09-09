366f2464062c5a4f3dd9a3afbec5c19da659909e
Yashwantyadav1 <yy888275@gmail.com>
2024-09-09 12:47:35 +0530

    Union of the array

---
+++ Array/vector/05_union.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5};
+    int sizea=5;
+    int brr[]={6,7,8,9,};
+    int sizeb=4;
+    vector<int>ans;
+    for(int i=0; i<sizea;i++){
+        ans.push_back(arr[i]);
+    }
+    for(int i=0; i<sizeb;i++){
+        ans.push_back(brr[i]);
+    }
+    cout<<"the union of the array is "<<endl;
+    for(int i=0;i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+
+
+}
