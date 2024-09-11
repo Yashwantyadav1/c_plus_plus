5fcaa8c03790b0250802f89c9a52294dfff9789f
Yashwantyadav1 <yy888275@gmail.com>
2024-09-11 19:12:26 +0530

    take user as input in  2D array

---
+++ Array/2D_Array/03_row_wise_input.cpp
@@ -0,0 +1,18 @@
+// take user as input in that program
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][4];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"print 2D array in row wise "<<endl;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
