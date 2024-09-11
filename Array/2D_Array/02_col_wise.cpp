72420882c7d84fd561fe542ec50422e2ad68d5b8
Yashwantyadav1 <yy888275@gmail.com>
2024-09-11 18:49:31 +0530

    2D array column wise declartion

---
+++ Array/2D_Array/02_col_wise.cpp
@@ -0,0 +1,13 @@
+// 2D array column wise declartion 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4][5]={{1,2,3,5,5},{7,8,9,5,4},{4,5,8,7,9},{16,15,44,12,5}};
+    for(int i=0; i<5;i++){
+        for(int j=0; j<4; j++){
+            cout<<arr[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
