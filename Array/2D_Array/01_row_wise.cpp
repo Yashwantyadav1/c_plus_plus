0d5f9261222c66f90913eed07bf7d74210252b4b
Yashwantyadav1 <yy888275@gmail.com>
2024-09-12 23:16:31 +0530

    Row wise acces in  2D array

--- Array/2D_Array/01_row_wise.cpp
+++ Array/2D_Array/01_row_wise.cpp
@@ -2,9 +2,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[3][4]={{1,2,3,7},{4,5,6,7},{1,2,5,6}};
+    int arr[3][5]={{1,2,3,7,5},{4,5,6,7,4},{1,2,5,6,9}};
     for(int i=0; i<3;i++){
-        for(int j=0; j<4;j++){
+        for(int j=0; j<5;j++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
