908f6ae4c8145cd8a242f3808831704b59d78abb
Yashwantyadav1 <yy888275@gmail.com>
2024-09-14 22:32:43 +0530

    2D vector intialization

--- Array/vector/2D_vector_intialize.cpp
+++ Array/vector/2D_vector_intialize.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int row=3;
     int col=4;
-    vector<vector<int> >arr(row,vector<int>(col,9));
+    vector<vector<int> >arr(row,vector<int>(col,101));
     for(int i=0;i<arr.size(); i++){
         for(int j=0; j<arr[i].size();j++){
             cout<<arr[i][j]<<" ";
