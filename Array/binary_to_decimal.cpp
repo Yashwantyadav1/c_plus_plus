3ab2ac7173a70d729a39ecbed380791ad051e26a
Yashwantyadav1 <yy888275@gmail.com>
2024-09-15 18:45:18 +0530

    binary to decimal conversion

---
+++ Array/binary_to_decimal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int binaryTodecimal(int n){
+    int decimal=0;
+    int i=0;
+    while(n){
+        int bit =n%10;
+        decimal=decimal+bit*pow(2,i++);
+        n /=10;
+
+    }
+    return decimal;
+}
+int main(){
+    int n;
+    cin>>n;
+    int decimalNo=binaryTodecimal(n);
+    cout<<decimalNo;
+}
