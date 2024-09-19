38e122492d147bea7dd3ea858e753a20ce8652c7
Yashwantyadav1 <yy888275@gmail.com>
2024-09-19 23:40:12 +0530

    In this  program we use stl library function to sort array and in Binary search  we use stl to search arrray

--- Array/Binary_Search/binary_arr.cpp
+++ Array/Binary_Search/binary_arr.cpp
@@ -4,6 +4,7 @@ using namespace std ;
 int main(){
     int arr[]={1,2,3,6,5,49,7,45,25};
     int size=9;
+    
     sort(arr,arr+size);// Fistly we sort the  given array then emlement the binary search (This role is applicable in unsorted element )
 
     if(binary_search(arr,arr+size,25)){
