4d43b04bc65b7ad430e6760c8cf4e8899effef29
Yashwantyadav1 <yy888275@gmail.com>
2024-09-26 12:23:44 +0530

    linearSearch in binary operation

---
+++ Array/Binary_Search/linear_search_binary.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int binarySearch(vector<int> arr,int target){
+    int s=0;
+    int e=arr.size()-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(arr[mid]==target)
+        return mid;
+        if(mid-1>=0 && arr[mid-1]==target)
+        return mid-1;
+        if(mid+1<arr.size() && arr[mid+1]==target)
+        return mid+1;
+        if(target>arr[mid]){
+            s=mid+2;
+        }else{
+            e=mid-2;
+        }
+        mid=s+(e-s)/2;
+    }
+    return -1;
+
+}
+int main(){
+    vector<int>arr{10,3,40,20,50,80,70};
+    int target=800;
+    int ans=binarySearch(arr,target);
+    cout<<"Index of target "<< target<<" "<< ans<<endl;
+}
