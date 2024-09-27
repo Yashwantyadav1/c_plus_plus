f25a2512c4b04a68570f017e7f7924688858a3db
Yashwantyadav1 <yy888275@gmail.com>
2024-09-27 11:52:25 +0530

    divide the given value using binary search

--- Array/Binary_Search/divide_binary_search.cpp
+++ Array/Binary_Search/divide_binary_search.cpp
@@ -1,36 +1,38 @@
 #include<iostream>
 using namespace std;
-int solve(int dividend, int divisor){
-    int s=0;
-    int e=abs(dividend);
-    int mid=s+(e-s)/2;
-    int ans=0;
-    while(s<=e){
-        if(abs(mid*divisor) == abs(dividend) ){
-            return mid;
+
+int solve(int dividend, int divisor) {
+    int s = 0;
+    int e = abs(dividend);
+    int mid = s + (e - s) / 2;
+    int ans = 0;
+
+    while (s <= e) {
+        if (abs(mid * divisor) == abs(dividend)) {
+            ans = mid;
+            break;
         }
-        if(abs(mid*divisor) > abs(dividend) ){
-           e=mid-1; 
+        if (abs(mid * divisor) > abs(dividend)) {
+            e = mid - 1;
         }
-        else{
-            ans=mid;
-            s=mid+1;
+        else {
+            ans = mid;
+            s = mid + 1;
         }
-        mid=s+(e-s)/2;
+        mid = s + (e - s) / 2;
     }
-    if((divisor<0 && dividend<0) || (divisor>0 && dividend))
-    return ans;
-    else{
+
+    // Handle the signs of the result
+    if ((divisor < 0 && dividend < 0) || (divisor > 0 && dividend > 0)) {
+        return ans;
+    } else {
         return -ans;
     }
-    //return ans;
-
 }
-int main(){
 
-    int dividend =-36;
-    int divisor= -22;
-    int ans=solve(dividend,divisor);
-    cout<<"Ans is : "<<ans<<endl;
-
+int main() {
+    int dividend = -21;
+    int divisor = 7;
+    int ans = solve(dividend, divisor);
+    cout << "Ans is : " << ans << endl;
 }
