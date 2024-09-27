a5789c864a6ef952757318d355ccbb5b0ea1c74b
Yashwantyadav1 <yy888275@gmail.com>
2024-09-27 11:45:08 +0530

    divide the number using the implimentation of binary search

--- Array/Binary_Search/divide_binary_search.cpp
+++ Array/Binary_Search/divide_binary_search.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int solve(int dividend, int divisor){
     int s=0;
-    int e=dividend;
+    int e=abs(dividend);
     int mid=s+(e-s)/2;
     int ans=0;
     while(s<=e){
-        if(mid*divisor == dividend ){
+        if(abs(mid*divisor) == abs(dividend) ){
             return mid;
         }
-        if(mid*divisor > dividend ){
+        if(abs(mid*divisor) > abs(dividend) ){
            e=mid-1; 
         }
         else{
@@ -18,13 +18,18 @@ int solve(int dividend, int divisor){
         }
         mid=s+(e-s)/2;
     }
+    if((divisor<0 && dividend<0) || (divisor>0 && dividend))
     return ans;
+    else{
+        return -ans;
+    }
+    //return ans;
 
 }
 int main(){
 
-    int dividend =36;
-    int divisor=7;
+    int dividend =-36;
+    int divisor= -22;
     int ans=solve(dividend,divisor);
     cout<<"Ans is : "<<ans<<endl;
 
