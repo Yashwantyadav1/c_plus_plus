a8c70651329c5e3b9329c4485b7335a9028b6bcf
Yashwantyadav1 <yy888275@gmail.com>
2024-10-15 22:46:21 +0530

    Implimentation of decimal to binary conversion

---
+++ decimal_to_bit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int decimalToBinaryMethod(int n){
+    int binaryno=0;
+    int i=0;
+    while(n>0){
+        int bits=n%2;
+        binaryno= bits*pow(10,i++)+binaryno;
+        n=n/2;
+    }
+    return binaryno;
+}
+int main(){
+    int n;
+    cin>>n;
+    int binary = decimalToBinaryMethod(n);
+    cout<<binary;
+}
